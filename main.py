e1a95a65fdb69ebfbf0c5c1531342233a6d3ddaa
Mario Garcia <mario.gtorrea@gmail.com>
2019-07-23 15:16:53 -0500

    Added deployment test4

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask
 import calculations
+import os
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World_again! {}'.format(calculations.inc(2))
+    return 'Hello, World_again! {} from commit: {}'.format(calculations.inc(2), os.env["COMMIT_ID"])
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
